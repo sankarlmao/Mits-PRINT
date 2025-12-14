5177d44725f666667d45ea790fc4e137a410cc65
athulcoder <athul.sin.cos.tan@gmail.com>
2025-12-14 22:57:32 +0530

    push plzzz

--- application/app/api/auth/[...nextauth]/route.js
+++ application/app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,10 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "../../../../lib/prisma";
 
 const TWO_WEEKS = 60 * 60 * 24 * 14;
 
 const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
 
   providers: [
     Credentials({
