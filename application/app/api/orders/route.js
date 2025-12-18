26e7c6f4aa83a4135aa2519552ee99f543989a6a
athulcoder <athul.sin.cos.tan@gmail.com>
2025-12-18 23:24:52 +0530

    changed the handler into POST of razorpay

--- application/app/api/orders/route.js
+++ application/app/api/orders/route.js
@@ -1,17 +1,27 @@
 import Razorpay from "razorpay";
 import { NextResponse } from "next/server";
 
+
+export async function POST(req) {
+   
+    if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_KEY_SECRET) {
+    return NextResponse.json(
+      { error: "Razorpay keys not configured" },
+      { status: 500 }
+    );
+  }
+    const body = await req.json();
+
+    const {amount} = body;
+    console.log(amount)
+
+
+
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-export async function POST(req) {
-   
-    const body = await req.json();
-
-    const {amount} = body;
-    console.log(amount)
 
   const order = await razorpay.orders.create({
     amount:amount, 
