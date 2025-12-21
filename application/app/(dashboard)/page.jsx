32770e76693994c25c41b6a355bdfcbb98728ac7
athulcoder <athul.sin.cos.tan@gmail.com>
2025-12-21 10:34:13 +0530

    dashbaord view upated

--- application/app/(dashboard)/page.jsx
+++ application/app/(dashboard)/page.jsx
@@ -9,10 +9,6 @@ const Homepage = async() => {
 
 
   return <section className='flex flex-col  items-center py-3 w-full  h-full  '>
-    <h1 className='text-xl font-sans'> 
-       UPLOAD FILE
-    </h1>
-
 
     <PrintUploader/>
 
